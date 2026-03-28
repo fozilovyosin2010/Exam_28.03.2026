549880d56b37db2aab68cc8d4517d9937fe65547
fozilovyosin2010 <umnovanika1@gmail.com>
2026-03-29 01:57:49 +0500

    design is ready

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,7 +22,25 @@ import Rec2 from "../img/Rec2.png";
 import Rec3 from "../img/Rec3.png";
 import Rec4 from "../img/Rec4.png";
 
+import FrameRed from "../img/FrameRed.svg";
+
+import triAng from "../img/triAng.svg";
+import imgAngle from "../img/imgAngle.png";
+
 const Home = () => {
+  const cardList = [
+    {
+      des: "You have ensured a systematic approach to our email strategy and helped us streamline all chaotic thoughts and ideas we had about our emails!",
+      name: "Natalia Pereldik",
+      role: "CEO, Funexpected Games",
+    },
+    {
+      des: "You have shown great professionalism and attention to detail when it comes to email marketing!",
+      name: "Ron Shalev",
+      role: "Founder, BYB Media",
+    },
+  ];
+
   const imgList = [levHaolam, shneider, aliexpress, laroche, papajohns, kfc];
 
   const ulList = [
@@ -49,7 +67,7 @@ const Home = () => {
   return (
     <div>
       <div
-        className="section1 bg-no-repeat bg-right bg-cover flex items-center p-[20px]"
+        className="section1 flex items-center p-[20px]"
         style={{
           backgroundImage: `url(${bgWave})`,
           backgroundPosition: "right",
@@ -77,7 +95,7 @@ const Home = () => {
       </div>
 
       <div
-        className="section3 bg-no-repeat bg-right bg-cover p-[20px] max-md:p-[10px]"
+        className="section3 p-[20px] max-md:p-[10px]"
         style={{
           backgroundImage: `url(${bgWave2})`,
           backgroundPosition: "left",
@@ -85,8 +103,8 @@ const Home = () => {
           backgroundSize: "50% 100%",
         }}
       >
-        <div className="bg-[#212121] shadow-[0_0_33px_#00000040] text-[#fff] flex justify-between m-[40px] gap-[50px] items-center rounded-[20px] p-[100px_80px] max-md:flex-col max-md:items-center max-md:gap-[25px] max-md:m-[20px] max-md:p-[25px_20px]">
-          <div className="font-[700] text-[128px] max-w-[831px] max-md:text-[28px] max-md:max-w-full">
+        <div className="bg-[#212121] shadow-[0_0_33px_#00000040] text-[#fff] flex flex-wrap justify-between m-[40px] gap-[50px] items-center rounded-[20px] p-[100px_80px] max-md:flex-col max-md:items-center max-md:gap-[25px] max-md:m-[20px] max-md:p-[25px_20px]">
+          <div className="font-[700] text-[100px] max-w-[831px] max-md:text-[28px] max-md:max-w-full">
             We are here to
             <span className="text-[#F5333F]"> help </span> when:
           </div>
@@ -99,14 +117,14 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="section4 flex justify-between items-center p-[20px_40px] gap-[30px] max-md:flex-col">
+      <div className="section4 flex justify-evenly   items-center p-[20px_40px] gap-[30px] max-md:flex-col">
         <div className="block1">
           <Swiper
             onSwiper={(swiper) => (swipeRef.current = swiper)}
             effect={"cards"}
             grabCursor={true}
             modules={[EffectCards]}
-            className="mySwiper max-h-[425px]"
+            className="mySwiper w-[425px]"
           >
             <SwiperSlide>
               <img src={Rec2} alt="" />
@@ -178,6 +196,135 @@ const Home = () => {
                   ></path>
                 </svg>
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${bgWave2})`,
+          backgroundPosition: "left",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "50% 100%",
+        }}
+        className="section5 p-[20px] max-md:p-[10px]"
+      >
+        <div className="bg-[#212121] shadow-[0_0_33px_#00000040] text-[#fff] m-[40px] rounded-[20px] p-[100px_80px] max-md:p-[25px_20px] max-md:m-[20px]">
+          <div className="block1 flex gap-[30px] max-lg:flex-wrap max-md:gap-[10px]">
+            <div className="text-[80px] font-[700] max-w-[1261px] max-lg:text-[28px]">
+              WE <span className="text-[#F5333F]">TAKE CARE</span> OF ALL YOUR
+              EMAIL MARKETING ACTIVITIES
+            </div>
+            <ul className="list-disc marker:text-[#F5333F] py-[20px] ml-[20px] flex flex-col justify-between">
+              <li>Planning</li>
+              <li>Design</li>
+              <li>Copywriting</li>
+              <li>Analitics</li>
+              <li>Delaverabilty</li>
+              <li>Automation</li>
+            </ul>
+          </div>
+          <div className="block2 font-[600] font-semibold text-[20px] text-[#FFFFFF] flex justify-between gap-[30px] max-md:flex-col">
+            <div>
+              <span className="text-[#F5333F]">For midsize businesses</span>, we
+              become your budget-friendly email marketing team, ensuring revenue
+              from emails at a fraction of in-house costs. Discover how it
+              works!
+            </div>
+            <div className="min-h-full w-[3px] bg-[#fff] max-md:w-full max-md:h-[3px]"></div>
+            <div>
+              <span className="text-[#F5333F]">
+                For email marketers at larger companies
+              </span>
+              , we enhance your productivity by handling execution, allowing you
+              to focus on strategy. Learn why we are your support, not your
+              competition!
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section6 relative p-5 pr-0 flex items-center overflow-x-hidden max-lg:flex-wrap max-lg:justify-center max-lg:pr-5">
+        <div className="block1 min-w-1/2">
+          <div className="font-[700] text-[80px] max-md:text-[28px] max-md:px-5">
+            TESTIMONIALS
+          </div>
+          <div className="flex flex-col gap-5">
+            {cardList.map((e, i) => {
+              return (
+                <div
+                  key={i}
+                  className="rounded-[20px] flex items-start shadow-[0_0_33px_#00000040] border border-[#D1D1D1] flex gap-5 p-5 m-5"
+                >
+                  <img src={FrameRed} alt="" />
+                  <div className="flex flex-col gap-3">
+                    <div className="text-[#212121] font-[400] text-[20px] max-w-[663px]">
+                      {e.des}
+                    </div>
+                    <div>
+                      <div className="text-[#F5333F] text-[18px] font-[500]">
+                        {e.name}
+                      </div>
+                      <div className="text-[#000000] text-[18px] font-[500]">
+                        {e.role}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="block2 relative right-0">
+          <img src={triAng} className="absolute top-0" alt="" />
+          <img
+            src={imgAngle}
+            alt=""
+            className="max-w-[700px] max-lg:max-w-full"
+          />
+        </div>
+      </div>
+      <div className="section7">
+        <div className="bg-[#212121] shadow-[0_0_33px_#00000040] text-[#fff] m-[40px] rounded-[20px] p-[100px_80px] flex justify-between gap-[40px] max-lg:flex-wrap max-md:p-[25px_20px] max-md:m-[20px]">
+          <div className="block1">
+            <h1 className="text-[80px] font-[700] max-w-[559px] max-md:text-[28px]">
+              SUBSCRIBE TO <span className="text-[#F5333F]">L.U.Y.E</span>
+            </h1>
+            <div className="text-[#F5333F]">Level Up Your Email Newsletter</div>
+          </div>
+          <div className="block2 max-w-[898px]">
+            <div className="miniBlock1 font-[400] flex flex-col gap-[30px]">
+              <div>
+                Our aim is to level up your email marketing to world-class
+                standards. We want to bring joy to your readers and deliver
+                excellent ROI for your business. That’s why we created our email
+                newsletter.
+              </div>
+              <div>
+                But here is the thing: it's not just about us promoting our
+                agency services (we promise!). In our emails, we share valuable
+                advice and practical tips based on our own experiences. Our goal
+                is to help you enhance your email marketing without breaking the
+                bank.
+              </div>
+              <div className="text-[#F5333F]">
+                Let's join forces and make your emails shine!
+              </div>
+            </div>
+            <div className="miniBlock2">
+              <div className="text-[40px] font-[700]">
+                Subscribe to L.U.Y.E.
+              </div>
+              <form className="max-w-full flex justify-between">
+                <input
+                  type="text"
+                  placeholder="Your Email"
+                  className="bg-[#fff] p-3 w-full rounded-l-[5px] placeholder:text-[#000] outline-none"
+                />
+                <button className="bg-[#F5333F] p-3 rounded-r-[5px]">
+                  SUBSCRIBE
+                </button>
+              </form>
+              <span>Согласие на получение писем?</span>
             </div>
           </div>
         </div>
